b46a091a33170260ebd9dca7eb58fe304cb5a0fe
liu_XiGua <liu_xigua@163.com>
2019-11-03 18:14:01 +0800

    创建person

---
+++ git/src/main/java/Person.java
@@ -0,0 +1,10 @@
+/**
+ * @ProjectName: learnSpace
+ * @Package: PACKAGE_NAME
+ * @ClassName: Person
+ * @Description: java类作用描述
+ * @Author: liuXiGua
+ * @Date: 2019/11/3 18:10
+ */
+public class Person {
+}
