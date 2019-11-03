9c3ed33e1c3e77f4aac13f2b8cb92e4a4f211923
liu_XiGua <liu_xigua@163.com>
2019-11-03 19:38:54 +0800

    创建user

---
+++ git/src/main/java/User.java
@@ -0,0 +1,13 @@
+/**
+ * @ProjectName: learnSpace
+ * @Package: PACKAGE_NAME
+ * @ClassName: User
+ * @Description: java类作用描述
+ * @Author: liuXiGua
+ * @Date: 2019/11/3 19:38
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("holle word");
+    }
+}
