5ff02e76a6e458b46afd4c8b7d687004d8315941
liu_XiGua <liu_xigua@163.com>
2019-11-03 19:48:17 +0800

    更新2

--- git/src/main/java/Car.java
+++ git/src/main/java/Car.java
@@ -1,13 +1,13 @@
 /**
  * @ProjectName: learnSpace
  * @Package: PACKAGE_NAME
- * @ClassName: User
+ * @ClassName: Car
  * @Description: java类作用描述
  * @Author: liuXiGua
- * @Date: 2019/11/3 19:38
+ * @Date: 2019/11/3 19:47
  */
-public class User {
+public class Car {
     public static void main(String[] args) {
-        System.out.println("holle word");
+        System.out.println("");
     }
 }
--- git/src/main/java/Person.java
+++ git/src/main/java/Person.java
@@ -9,4 +9,8 @@
 public class Person {
 
     private String name;
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
 }
