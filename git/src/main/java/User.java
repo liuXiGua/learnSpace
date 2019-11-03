06f381e1c860b33c7734b2622cd04c87cfde2749
liu_XiGua <liu_xigua@163.com>
2019-11-03 17:50:28 +0800

    创建User

---
+++ git/src/main/java/User.java
@@ -0,0 +1,10 @@
+/**
+ * @ProjectName: git-demo01
+ * @Package: PACKAGE_NAME
+ * @ClassName: User
+ * @Description: java类作用描述
+ * @Author: liuXiGua
+ * @Date: 2019/11/3 17:48
+ */
+public class User {
+}
