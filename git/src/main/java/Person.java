93527aca2fc8ca59ede8d5c625b8579886a92f8f
liuXiGua <13528885155@163.com>
2019-11-05 23:15:23 +0800

    xiugai

--- git/src/main/java/Person.java
+++ git/src/main/java/Person.java
@@ -11,11 +11,20 @@ public class Person {
     private String name;
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        // 定义数组，存储3个元素
+        int[] arr = new int[3];
+        //数组索引进行赋值
+        arr[0] = 5;
+        arr[1] = 6;
+        arr[2] = 7;
+        //输出3个索引上的元素值
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        //定义数组变量arr2，将arr的地址赋值给arr2
+        int[] arr2 = arr;
+        arr2[1] = 9;
+        System.out.println(arr[1]);
     }
 
-
-    private void say(){
-        System.out.println(name);
-    }
 }
